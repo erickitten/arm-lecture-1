88399bbdb1cd701aef2f7071df3149dd6a03ce78
Che-Kang Wu <erickitten@gmail.com>
2015-03-15 02:25:56 +0800

    add working fast Fibonacci solver
    the algorithm is based on :
        F(2m) = F(m)[2F(m+1) - F(m)]
        F(2m+1) = F(m+1)^2 + F(m)^2
    m starts at 1, and doubles through every digit n(binary) has
    if non-leading 1 is encountered, m "moves up" by 1
    for example, n=10 :
    n       1010        1010        1010        1010
    F(m)    ^   F(1)     ^  F(2)      ^ F(4+1)     ^F(10)

--- lab-1f/fib.s
+++ lab-1f/fib.s
@@ -9,23 +9,51 @@
 	.type fibonacci, function
 
 fibonacci:
-	push { r4, r5, r6, lr}
+	push { r4, r5, r6, r7, r8, lr}
 
     cmp r0,#0                   @n<=0, f(n)=0
     ble .L3
-    cmp r0,#1                   @n=1, f(n)=1
-    beq .L4
 
+    @invariants ,m(current Fibonacci) start as 1
+    mov r5,#1                   @r5 = F(m)
+    mov r6,#1                   @r6 = F(m+1)
 
+    clz r4,r0                   @count leading zero of r0
+                                @really surprised this operation exists
+    rsbs r4,r4,#31              @r4 = 31-r4 ("non-zero" part)
+    beq exit                    @when r0 = 1
+loop:
+    
+    @doubling Fibonacci here
+    rsb r7,r5,r6,LSL #1     @r7 = r6<<1-r5
+    mul r7,r5,r7            @r7 = r5*r7 = r5*(2*r6-r5) = F(2m)
 
-exi:
-    mov r0,r6
-	pop { r4, r5, r6, pc}		@EPILOG
+    mul r8,r5,r5            @r8 = r5^2
+    mla r8,r6,r6,r8         @r8 = r6^2 + r5^2 = F(2m+1)
 
+    @update a,b : m*=2
+    mov r5,r7
+    mov r6,r8
+
+    sub r4,r4,#1
+
+    @advance one conditionally
+    lsr r7,r0,r4
+    tst r7,#1
+    beq no_adv
+    add r7,r5,r6
+    mov r5,r6               @r7 = F(m+1)
+    mov r6,r7               @r8 = F(m+2) ,m++
+no_adv:
+    cbz r4,exit
+    b loop
+
+exit:
+    mov r0,r5
+	pop { r4, r5, r6, r7, r8, pc}
 .L3:
-	mov r0, #0			        @ R0 = 0
-.L4:
-	pop { r4, r5, r6, pc}		@ EPILOG
+	mov r0, #0			            @ R0 = 0
+	pop { r4, r5, r6, r7, r8, pc}
 
 	.size fibonacci, .-fibonacci
 	.end
